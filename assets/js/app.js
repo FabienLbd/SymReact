9a6e4e0e85690e4906a0c9ef0c9f7d5b2213cc5d
fabienLbd <fabien.labedade@gmail.com>
2020-03-20 10:13:06 +0100

    authentification done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,43 @@
-import React from "react";
+import React, {useState, useContext} from 'react';
 import ReactDOM from "react-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/Homepage";
-import {HashRouter, Switch, Route} from "react-router-dom";
+import {HashRouter, Switch, Route, withRouter, Redirect} from "react-router-dom";
 import CustomersPage from "./pages/CustomersPage";
 import InvoicesPage from "./pages/InvoicesPage";
-
+import LoginPage from "./pages/LoginPage";
+import AuthAPI from './services/authAPI';
+import AuthContext from './contexts/AuthContext';
+import PrivateRoute from "./components/PrivateRoute";
 
 import '../css/app.css';
 
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+AuthAPI.setup();
 
 const App = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(
+        AuthAPI.isAuthenticated()
+    );
+    const Navbarwithrouter = withRouter(Navbar);
+
     return (
-        <HashRouter>
-            <Navbar/>
-            <main className="container pt-5">
-                <Switch>
-                    <Route path="/invoices" component={InvoicesPage}/>
-                    <Route path="/customers" component={CustomersPage}/>
-                    <Route path="/" component={HomePage}/>
-                </Switch>
-            </main>
-        </HashRouter>
+        <AuthContext.Provider value={{
+            isAuthenticated,
+            setIsAuthenticated
+        }}>
+            <HashRouter>
+                <Navbarwithrouter />
+                <main className="container pt-5">
+                    <Switch>
+                        <Route path="/login" component={LoginPage} />
+                        <PrivateRoute path="/invoices" component={InvoicesPage} />
+                        <PrivateRoute path="/customers" component={CustomersPage} />
+                        <Route path="/" component={HomePage}/>
+                    </Switch>
+                </main>
+            </HashRouter>
+        </AuthContext.Provider>
     );
 };
 
---
+++ assets/js/services/authAPI.js
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import jwtDecode from 'jwt-decode';
+
+/**
+ * Déconnexion (supression du token du local storage et sur axios)
+ */
+function logout() {
+    window.localStorage.removeItem("authToken");
+    delete axios.defaults.headers["Authorization"];
+}
+
+/**
+ * Requête HTTP d'authentification et stockage du token dans le storage et sur axios
+ * @param {object} credentials
+ */
+function authenticate(credentials) {
+    return axios
+        .post("http://localhost:8000/api/login_check", credentials)
+        .then(response => response.data.token)
+        .then(token => {
+            //Je stocke le token dans mon localStorage
+            window.localStorage.setItem("authToken", token);
+            //On previens axios qu'on à maintenant un header par defaut sur toutes nos futures requetes Http
+            setAxiosToken(token);
+        });
+}
+
+/**
+ * Positionne le token JWT sur axios
+ * @param {string} token Le token JWT
+ */
+function setAxiosToken(token) {
+    axios.defaults.headers["Authorization"] = "Bearer " + token;
+}
+
+/**
+ * Mise en place lors du chargement de l'application
+ */
+function setup() {
+    const token = window.localStorage.getItem("authToken");
+    if (token) {
+        const {exp: expiration} = jwtDecode(token);
+        if (expiration * 1000 > new Date().getTime()) {
+            setAxiosToken(token);
+        }
+    }
+}
+
+/**
+ * Permet de savoir si on est authentifié ou pas
+ * @returns {boolean}
+ */
+function isAuthenticated() {
+    const token = window.localStorage.getItem("authToken");
+    if (token) {
+        const {exp: expiration} = jwtDecode(token);
+        if (expiration * 1000 > new Date().getTime()) {
+            return true;
+        }
+        return false;
+    }
+    return false;
+}
+
+export default {
+    authenticate,
+    logout,
+    setup,
+    isAuthenticated
+}
