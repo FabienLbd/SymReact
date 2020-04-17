7fb0ccb038092e6e0acf3dd38044da75eb416f2c
fabienLbd <fabien.labedade@gmail.com>
2020-04-17 14:40:04 +0200

    fix register form and add meta viewport to base.html.twig

--- assets/js/pages/RegisterPage.jsx
+++ assets/js/pages/RegisterPage.jsx
@@ -67,7 +67,7 @@ const RegisterPage = ({ history }) => {
                 <h1>Inscription</h1>
                 <form onSubmit={handleSubmit}>
                     <div className="row">
-                        <div className="col-6">
+                        <div className="col-md-6">
                             <Field
                                 isRequired={true}
                                 name="firstname"
@@ -78,118 +78,140 @@ const RegisterPage = ({ history }) => {
                                 value={user.firstname}
                                 error={errors.firstname}
                             />
-                            <Field
-                                isRequired={true}
-                                name="lastname"
-                                type="text"
-                                placeholder="Votre nom de famille"
-                                label="Nom de famille"
-                                onChange={handleChange}
-                                value={user.lastname}
-                                error={errors.lastname}
-                            />
-                            <Field
-                                isRequired={true}
-                                name="email"
-                                type="email"
-                                placeholder="Votre email"
-                                label="Adresse email"
-                                onChange={handleChange}
-                                value={user.email}
-                                error={errors.email}
-                            />
-                            <Field
-                                isRequired={true}
-                                name="address"
-                                type="text"
-                                placeholder="Entrez votre adresse"
-                                label="Adresse"
-                                onChange={handleChange}
-                                value={user.address}
-                                error={errors.address}
-                            />
                         </div>
-                        <div className="col-6">
-                            <Field
-                                isRequired={true}
-                                name="postalCode"
-                                type="text"
-                                placeholder="Entrez votre code postal"
-                                label="Code postal"
-                                onChange={handleChange}
-                                value={user.postalCode}
-                                error={errors.postalCode}
-                            />
-                            <Field
-                                isRequired={true}
-                                name="city"
-                                type="text"
-                                placeholder="Entrez votre ville"
-                                label="Ville"
-                                onChange={handleChange}
-                                value={user.city}
-                                error={errors.city}
-                            />
-                            <Field
-                                isRequired={true}
-                                name="phone"
-                                type="text"
-                                placeholder="Entrez votre numéro de téléphone"
-                                label="Téléphone"
-                                onChange={handleChange}
-                                value={user.phone}
-                                error={errors.phone}
-                            />
-                            <Field
-                                isRequired={true}
-                                name="numTVA"
-                                type="number"
-                                placeholder="Entrez votre numéro de TVA"
-                                label="Numéro de TVA"
-                                onChange={handleChange}
-                                value={user.numTVA}
-                                error={errors.numTVA}
-                            />
+                            <div className="col-md-6">
+                                <Field
+                                    isRequired={true}
+                                    name="lastname"
+                                    type="text"
+                                    placeholder="Votre nom de famille"
+                                    label="Nom de famille"
+                                    onChange={handleChange}
+                                    value={user.lastname}
+                                    error={errors.lastname}
+                                />
+                            </div>
                         </div>
-                        <div className="col-12">
-                            <Field
-                                isRequired={true}
-                                name="company"
-                                type="text"
-                                placeholder="Entrez le nom de votre société"
-                                label="Société"
-                                onChange={handleChange}
-                                value={user.company}
-                                error={errors.company}
-                            />
+                        <div className="row">
+                            <div className="col-md-6">
+                                <Field
+                                    isRequired={true}
+                                    name="email"
+                                    type="email"
+                                    placeholder="Votre email"
+                                    label="Adresse email"
+                                    onChange={handleChange}
+                                    value={user.email}
+                                    error={errors.email}
+                                />
+                            </div>
+                            <div className="col-md-6">
+                                <Field
+                                    isRequired={true}
+                                    name="address"
+                                    type="text"
+                                    placeholder="Entrez votre adresse"
+                                    label="Adresse"
+                                    onChange={handleChange}
+                                    value={user.address}
+                                    error={errors.address}
+                                />
+                            </div>
                         </div>
-                        <div className="col-6">
-                            <Field
-                                isRequired={true}
-                                name="password"
-                                type="password"
-                                placeholder="Votre mot de passe"
-                                label="Mot de passe"
-                                onChange={handleChange}
-                                value={user.password}
-                                error={errors.password}
-                            />
+                        <div className="row">
+                            <div className="col-md-6">
+                                <Field
+                                    isRequired={true}
+                                    name="city"
+                                    type="text"
+                                    placeholder="Entrez votre ville"
+                                    label="Ville"
+                                    onChange={handleChange}
+                                    value={user.city}
+                                    error={errors.city}
+                                />
+                            </div>
+                            <div className="col-md-6">
+                                <Field
+                                    isRequired={true}
+                                    name="postalCode"
+                                    type="text"
+                                    placeholder="Entrez votre code postal"
+                                    label="Code postal"
+                                    onChange={handleChange}
+                                    value={user.postalCode}
+                                    error={errors.postalCode}
+                                />
+                            </div>
                         </div>
-                        <div className="col-6">
-                            <Field
-                                name="passwordConfirm"
-                                type="password"
-                                placeholder="Confirmez votre super mot de passe"
-                                label="Confirmation du mot de passe"
-                                onChange={handleChange}
-                                value={user.passwordConfirm}
-                                error={errors.passwordConfirm}
-                            />
+                        <div className="row">
+                            <div className="col-md-6">
+                                <Field
+                                    isRequired={true}
+                                    name="phone"
+                                    type="text"
+                                    placeholder="Entrez votre numéro de téléphone"
+                                    label="Téléphone"
+                                    onChange={handleChange}
+                                    value={user.phone}
+                                    error={errors.phone}
+                                />
+                            </div>
+                            <div className="col-md-6">
+                                <Field
+                                    isRequired={true}
+                                    name="numTVA"
+                                    type="number"
+                                    placeholder="Entrez votre numéro de TVA"
+                                    label="Numéro de TVA"
+                                    onChange={handleChange}
+                                    value={user.numTVA}
+                                    error={errors.numTVA}
+                                />
+                            </div>
                         </div>
-                    </div>
+                        <div className="row">
+                            <div className="col-12">
+                                <Field
+                                    isRequired={true}
+                                    name="company"
+                                    type="text"
+                                    placeholder="Entrez le nom de votre société"
+                                    label="Société"
+                                    onChange={handleChange}
+                                    value={user.company}
+                                    error={errors.company}
+                                />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <Field
+                                    isRequired={true}
+                                    name="password"
+                                    type="password"
+                                    placeholder="Votre mot de passe"
+                                    label="Mot de passe"
+                                    onChange={handleChange}
+                                    value={user.password}
+                                    error={errors.password}
+                                />
+                            </div>
+                            <div className="col-md-6">
+                                <Field
+                                    name="passwordConfirm"
+                                    type="password"
+                                    placeholder="Confirmez votre super mot de passe"
+                                    label="Confirmation du mot de passe"
+                                    onChange={handleChange}
+                                    value={user.passwordConfirm}
+                                    error={errors.passwordConfirm}
+                                />
+                            </div>
+                        </div>
                     <div className="form-group d-flex justify-content-end">
-                        <button type="submit" className="btn btn-success">Confirmation</button>
                         <Link to="/login" className="btn btn-link">J'ai déjà un compte</Link>
+                        <button type="submit" className="btn btn-success">Confirmation</button>
                     </div>
                 </form>
             </div>
