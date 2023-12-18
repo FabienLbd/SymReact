e5dcebef5fb6becc449ab6765b115ece393a26c9
flabedade <fabien.labedade@omnilog.fr>
2023-12-18 11:35:56 +0100

    Upgrade

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,95 +1,101 @@
 <?php
 
+//class AppFixtures extends Fixture
+//{
+//    /**
+//     * @var UserPasswordEncoderInterface
+//     */
+//    private $encoder;
+//
+//    public function __construct(UserPasswordEncoderInterface $encoder)
+//    {
+//        $this->encoder = $encoder;
+//    }
+//
+//    public function load(ObjectManager $manager)
+//    {
+//        $faker = Factory::create('fr_FR');
+//
+//        for ($u = 0 ; $u < 10 ; $u++) {
+//            $user = new User();
+//
+//            $chrono = 1;
+//
+//            $hash = $this->encoder->encodePassword($user, 'password');
+//
+//            $user
+//                ->setFirstname($faker->firstName)
+//                ->setLastname($faker->lastName)
+//                ->setEmail($faker->email)
+//                ->setPassword($hash)
+//                ->setCompany($faker->company)
+//                ->setAddress($faker->address)
+//                ->setPostalCode($faker->postcode)
+//                ->setCity($faker->city)
+//                ->setNumTVA($faker->randomDigit)
+//                ->setPhone($faker->phoneNumber)
+//            ;
+//            $manager->persist($user);
+//
+//            for ($i = 0 ; $i < 30; $i++) {
+//                $customer = new Customer();
+//                $customer
+//                    ->setFirstname($faker->firstName())
+//                    ->setLastname($faker->lastName())
+//                    ->setCompany($faker->company())
+//                    ->setEmail($faker->email)
+//                    ->setUser($user)
+//                    ->setAddress($faker->address)
+//                    ->setPostalCode($faker->postcode)
+//                    ->setCity($faker->city)
+//                ;
+//                $manager->persist($customer);
+//
+//                for ($k = 0 ; $k < mt_rand(5, 20); $k++ ) {
+//                    $invoice = new Invoice();
+//                    $invoice
+//                        ->setAmount($faker->randomFloat(2, 250, 5000))
+//                        ->setSentAt($faker->dateTimeBetween('- 6 months'))
+//                        ->setStatus($faker->randomElement(['SENT', 'PAID', 'CANCELLED']))
+//                        ->setCustomer($customer)
+//                        ->setChrono($chrono)
+//                        ->setFee($faker->randomFloat(2,150, 200))
+//                    ;
+//                    $chrono++;
+//
+//                    $manager->persist($invoice);
+//                }
+//            }
+//        }
+//        $admin = new User();
+//        $admin->setFirstname('fabien')
+//            ->setLastname('labedade')
+//            ->setNumTVA(0)
+//            ->setCompany('WCS')
+//            ->setEmail('fabien.labedade@gmail.com')
+//            ->setAddress('1 allée quiétude')
+//            ->setPhone('0662434908')
+//            ->setCity('Merignac')
+//            ->setPostalCode('33700')
+//            ->setPassword($this->encoder->encodePassword($admin, 'password'))
+//            ->setRoles([User::ROLE_ADMIN]);
+//        $manager->persist($admin);
+//
+//        $manager->flush();
+//    }
+//}
+
 namespace App\DataFixtures;
 
-use App\Entity\Customer;
-use App\Entity\Invoice;
-use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
-use Faker\Factory;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    /**
-     * @var UserPasswordEncoderInterface
-     */
-    private $encoder;
-
-    public function __construct(UserPasswordEncoderInterface $encoder)
+    public function load(ObjectManager $manager): void
     {
-        $this->encoder = $encoder;
-    }
-
-    public function load(ObjectManager $manager)
-    {
-        $faker = Factory::create('fr_FR');
-
-        for ($u = 0 ; $u < 10 ; $u++) {
-            $user = new User();
-
-            $chrono = 1;
-
-            $hash = $this->encoder->encodePassword($user, 'password');
-
-            $user
-                ->setFirstname($faker->firstName)
-                ->setLastname($faker->lastName)
-                ->setEmail($faker->email)
-                ->setPassword($hash)
-                ->setCompany($faker->company)
-                ->setAddress($faker->address)
-                ->setPostalCode($faker->postcode)
-                ->setCity($faker->city)
-                ->setNumTVA($faker->randomDigit)
-                ->setPhone($faker->phoneNumber)
-            ;
-            $manager->persist($user);
-
-            for ($i = 0 ; $i < 30; $i++) {
-                $customer = new Customer();
-                $customer
-                    ->setFirstname($faker->firstName())
-                    ->setLastname($faker->lastName())
-                    ->setCompany($faker->company())
-                    ->setEmail($faker->email)
-                    ->setUser($user)
-                    ->setAddress($faker->address)
-                    ->setPostalCode($faker->postcode)
-                    ->setCity($faker->city)
-                ;
-                $manager->persist($customer);
-
-                for ($k = 0 ; $k < mt_rand(5, 20); $k++ ) {
-                    $invoice = new Invoice();
-                    $invoice
-                        ->setAmount($faker->randomFloat(2, 250, 5000))
-                        ->setSentAt($faker->dateTimeBetween('- 6 months'))
-                        ->setStatus($faker->randomElement(['SENT', 'PAID', 'CANCELLED']))
-                        ->setCustomer($customer)
-                        ->setChrono($chrono)
-                        ->setFee($faker->randomFloat(2,150, 200))
-                    ;
-                    $chrono++;
-
-                    $manager->persist($invoice);
-                }
-            }
-        }
-        $admin = new User();
-        $admin->setFirstname('fabien')
-            ->setLastname('labedade')
-            ->setNumTVA(0)
-            ->setCompany('WCS')
-            ->setEmail('fabien.labedade@gmail.com')
-            ->setAddress('1 allée quiétude')
-            ->setPhone('0662434908')
-            ->setCity('Merignac')
-            ->setPostalCode('33700')
-            ->setPassword($this->encoder->encodePassword($admin, 'password'))
-            ->setRoles([User::ROLE_ADMIN]);
-        $manager->persist($admin);
+        // $product = new Product();
+        // $manager->persist($product);
 
         $manager->flush();
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
+<<<<<<< ours
 require("dotenv").config();
 
 var Encore = require('@symfony/webpack-encore');
+=======
+const Encore = require('@symfony/webpack-encore');
+>>>>>>> theirs
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -13,21 +17,19 @@ Encore
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
     .setPublicPath('/build')
-    // only needed for CDN's or sub-directory deploy
+    // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
 
     /*
      * ENTRY CONFIG
      *
-     * Add 1 entry for each "page" of your app
-     * (including one that's included on every page - e.g. "app")
-     *
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
+    .addEntry('app', './assets/app.js')
+
+    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
+    .enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -49,10 +51,22 @@ Encore
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
 
+<<<<<<< ours
     // enables @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
         config.corejs = 3;
+=======
+    // configure Babel
+    // .configureBabel((config) => {
+    //     config.plugins.push('@babel/a-babel-plugin');
+    // })
+
+    // enables and configure @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = '3.23';
+>>>>>>> theirs
     })
 
     // enables Sass/SCSS support
@@ -61,12 +75,16 @@ Encore
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
 
+    // uncomment if you use React
+    //.enableReactPreset()
+
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+<<<<<<< ours
 
     // uncomment if you use API Platform Admin (composer req api-admin)
     .enableReactPreset()
@@ -75,5 +93,8 @@ Encore
 Encore.configureDefinePlugin(options => {
     options["process.env"].API_URL = process.env.API_URL;
 });
+=======
+;
+>>>>>>> theirs
 
 module.exports = Encore.getWebpackConfig();
